2b455f850ee7185c51ab001e6e7a31f8dd8c4ec1
Robin6934 <139586458+Robin6934@users.noreply.github.com>
2025-02-06 21:04:06 +0100

    added page to create the image frame

---
+++ Photobox.Web/Photobox.Web.Client/Pages/Components/ImageObject.cs
@@ -0,0 +1,12 @@
+﻿namespace Photobox.Web.Client.Pages.Components;
+
+public class ImageObject
+{
+    public string Type { get; set; } = ""; // "Text" or "Image"
+    public string Content { get; set; } = ""; // Image URL or Text
+    public int X { get; set; } = 0;
+    public int Y { get; set; } = 0;
+    public int Width { get; set; } = 50;
+    public int Height { get; set; } = 50;
+    public int FontSize { get; set; } = 16;
+}
--- Photobox.Web/Photobox.Web.Client/Program.cs
+++ Photobox.Web/Photobox.Web.Client/Program.cs
@@ -3,14 +3,14 @@ using Serilog;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .WriteTo.BrowserConsole()
-                .WriteTo.Seq("http://localhost:5341")
-                .Enrich.FromLogContext()
-                .Enrich.WithEnvironmentName()
-                .Enrich.WithMachineName()
-                .Enrich.WithProperty("Source", "Client")
-                .CreateLogger();
+// Log.Logger = new LoggerConfiguration()
+//                 .MinimumLevel.Debug()
+//                 .WriteTo.BrowserConsole()
+//                 .WriteTo.Seq("http://localhost:5341")
+//                 .Enrich.FromLogContext()
+//                 .Enrich.WithEnvironmentName()
+//                 .Enrich.WithMachineName()
+//                 .Enrich.WithProperty("Source", "Client")
+//                 .CreateLogger();
 
 await builder.Build().RunAsync();
