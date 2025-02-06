1d125e7a3c3eed3564117fc6454922caf9ae5e56
Robin6934 <139586458+Robin6934@users.noreply.github.com>
2025-02-06 21:04:06 +0100

    added page to create the image frame

---
+++ Photobox.Web/Photobox.Web/wwwroot/site.js
@@ -0,0 +1,12 @@
+﻿window.attachMouseEvents = (dotNetHelper) => {
+    document.addEventListener("mousemove", (event) => {
+        dotNetHelper.invokeMethodAsync("OnMouseMove", {
+            clientX: event.clientX,
+            clientY: event.clientY
+        });
+    });
+
+    document.addEventListener("mouseup", () => {
+        dotNetHelper.invokeMethodAsync("StopDrag");
+    });
+};
