7a3a9b68c2a70b7aa5a4272dcb4844278fbe9ecf
Robin6934 <139586458+Robin6934@users.noreply.github.com>
2025-06-16 23:36:55 +0200

    added nuget packages for tests

---
+++ src/Photobox.Web/Photobox.Web/Migrations/20250615084703_addedEventcodeToGallery.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Photobox.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class addedEventcodeToGallery : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<Guid>(
+                name: "EventId",
+                table: "Images",
+                type: "uuid",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uuid",
+                oldNullable: true
+            );
+
+            migrationBuilder.AddColumn<string>(
+                name: "EventCode",
+                table: "Events",
+                type: "text",
+                nullable: false,
+                defaultValue: ""
+            );
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Events_EventCode",
+                table: "Events",
+                column: "EventCode",
+                unique: true
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(name: "IX_Events_EventCode", table: "Events");
+
+            migrationBuilder.DropColumn(name: "EventCode", table: "Events");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "EventId",
+                table: "Images",
+                type: "uuid",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uuid"
+            );
+        }
+    }
+}
